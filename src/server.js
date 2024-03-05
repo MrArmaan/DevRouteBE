875965c2d2eb985f2b8789e98ed3d01fb1e9813d
Mohammed Armaan <mr_mohammed_armaan@icloud.com>
2024-03-05 12:00:10 +0000

    Enhancement: S2BE setup with health route

---
+++ src/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Server is running" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
