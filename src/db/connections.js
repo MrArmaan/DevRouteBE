3f0b0832620e0a9195bf6887ba507ffb3416bd05
Michael Gregory <mr.m.gregory@outlook.com>
2024-03-06 12:39:46 +0000

    enhancement: debug of server.js

---
+++ src/db/connections.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.MYSQL_URI);
+
+sequelize.authenticate();
+
+console.log("db  is working");
+
+module.exports = sequelize;
