6e9d7bf9c6b114686b995607ea03f61950dbc0dd
Alex Homoky <zhomoky@gmail.com>
2024-03-06 15:29:15 +0000

    enhancement: BE13GetAllUsers

--- src/users/routes.js
+++ src/users/routes.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const userRouter = Router();
 
-const { signup, login } = require("./controllers");
+const { signup, login, getAllUsers } = require("./controllers");
 
 const { hashPass, comparePass } = require("../middleware/auth");
 
 userRouter.post("/users/signup", signup);
 
 userRouter.post("/users/login", login);
+
+userRouter.get("/users/getAllUsers", getAllUsers);
 
 module.exports = userRouter;
