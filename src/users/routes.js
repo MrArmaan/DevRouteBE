9f0ef6a7572a54fc4571bd4ff24a7380dc8a5584
Mohammed Armaan <mr_mohammed_armaan@icloud.com>
2024-03-06 14:47:47 +0000

    enhancement: Compare and hashpass route added

--- src/users/routes.js
+++ src/users/routes.js
@@ -3,6 +3,8 @@ const userRouter = Router();
 
 const { signup } = require("./controllers");
 
+const { hashPass, comparePass } = require("../middleware/auth");
+
 userRouter.post("/users/signup", signup);
 
 module.exports = userRouter;
