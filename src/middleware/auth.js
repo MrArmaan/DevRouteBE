0fccbecad236e10ac83ef2e9f5ca7fcf07486689
Michael Gregory <mr.m.gregory@outlook.com>
2024-03-06 13:51:27 +0000

    chore: premerge commit

---
+++ src/middleware/auth.js
@@ -0,0 +1,11 @@
+const bcrypt = require("bcrypt");
+
+const User = require("../users/model");
+
+const saltRounds = parseInt(process.env.SALT_ROUNDS);
+
+const hashPass = async (req, res, next) => {
+  try {
+    console.log("");
+  } catch (error) {}
+};
